85295f736783cbd89093d83da383f9a6f23dcd2f
RyanPrax <140210909+RyanPrax@users.noreply.github.com>
2024-11-30 23:50:46 -0600

    solved day 1 problem 1

---
+++ day_1/day_1_1.py
@@ -0,0 +1,14 @@
+with open('input.txt') as f:
+    lines = f.readlines()
+    list1 = []
+    list2 = []
+    sum = 0 
+    for list in lines:
+        list1.append(list[:5])
+        list2.append(list[8:])
+    list1.sort()
+    list2.sort()
+    for i in range(len(list1)):
+        difference = abs(int(list1[i]) - int(list2[i]))
+        sum += difference
+    print(sum)
